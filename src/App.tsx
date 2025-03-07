66e312a44465a955b417a3f09607cd2e4a06d441
Mathias Rendon <mathiassaid7@gmail.com>
2025-03-07 16:07:57 -0500

    feat: import header and form components

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Header from '@components/Header'
+import Form from '@components/Form'
+import lineTop from '@public/images/patternLineTop.svg'
+import lineBottom from '@public/images/patternBottomDesktop.svg'
+
+function App() {
+  return (
+    <>
+    <img src={lineTop} alt="" className="absolute top-0 right-0"/>
+    <img src={lineBottom} alt="" className="absolute bottom-0 left-0"/>
+    <Header/>
+    <Form />
+    </>
+  )
+}
+
+export default App
