408d8dfc25e64924309819efd7884a479245f701
Mathias Rendon <mathiassaid7@gmail.com>
2025-03-08 10:36:52 -0500

    feat(routes): add /ticket and its element

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,16 @@
 import App from './App'
 import DefaultError from '@components/DefaultError'
+import Ticket from '@components/Ticket'
 
 const routes = [
     {
         path: '/',
         element: <App />,
+        errorElement: <DefaultError />
+    },
+    {
+        path: '/ticket',
+        element: <Ticket />,
         errorElement: <DefaultError />
     }
 ]
