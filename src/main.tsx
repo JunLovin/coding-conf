a1200332661768995358f80d132b15a6ed950d8f
Mathias Rendon <mathiassaid7@gmail.com>
2025-03-11 11:18:25 -0500

    edit: wrap app in context

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
+import { UserProvider } from '@components/UserContext'
 import './index.css'
 import routes from './routes'
 
@@ -8,6 +9,8 @@ const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <UserProvider>
+      <RouterProvider router={router}/>
+    </UserProvider>
   </StrictMode>,
 )
