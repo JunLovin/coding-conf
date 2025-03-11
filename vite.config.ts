90994205bdc09401dbd78f80a13f20864cdd516c
Mathias Rendon <mathiassaid7@gmail.com>
2025-03-11 11:33:47 -0500

    fix: import tailwindcss and add it to plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
