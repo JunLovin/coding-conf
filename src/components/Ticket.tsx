fc2363461426c5bea8cbbcd4d3b4dd77d02bfd62
Mathias Rendon <mathiassaid7@gmail.com>
2025-03-11 11:19:27 -0500

    feat: import userData and use the values in the componet

--- src/components/Ticket.tsx
+++ src/components/Ticket.tsx
@@ -1,23 +1,35 @@
+import { useContext } from 'react'
+import { UserContext } from '@components/UserContext'
 import Header from '@components/Header'
 import Logo from '@public/images/logo-full.svg'
-import Pfp from '@public/images/image-avatar.jpg'
+import GithubLogo from '@public/images/icon-github.svg'
+import lineTop from '@public/images/patternLineTop.svg'
+import lineBottom from '@public/images/patternBottomDesktop.svg'
 
-function Ticket({ name, email, username }) {
+function Ticket() {
+    const { userData } = useContext(UserContext)
+    const { name, email, username, pfp } = userData
 
     const handleRandom = ():number => {
         return Math.floor(Math.random() * 1000000)
     }
 
+    const handleGithub = (username) => {
+        window.open(`https://github.com/${username}`)
+    }
+
     return (
         <>
         <Header />
+        <img src={lineTop} alt="" className="absolute top-0 right-0"/>
+        <img src={lineBottom} alt="" className="absolute bottom-0 left-0"/>
         <div className="ticket-container h-[90dvh]">
             <div className="ticket flex flex-col justify-around items-center w-full h-[80%]">
                 <div className="ticket-header">
-                    <h2 className="text-6xl text-white font-extrabold text-center leading-normal">Congrats, <span className="bg-gradient-to-r from-orange-600 to-white bg-clip-text text-transparent">Jonatan Kristof!</span></h2>
+                    <h2 className="text-6xl text-white font-extrabold text-center leading-normal">Congrats, <span className="bg-gradient-to-r from-orange-600 to-white bg-clip-text text-transparent">{name}!</span></h2>
                     <h2 className="text-5xl text-white font-extrabold text-center leading-normal">Your ticket is ready.</h2>
                     <div className="ticket-header-text">
-                        <p className="text-white text-[1.1rem] text-center leading-normal mt-6 w-2xl m-auto">We've emailed your ticket to <span className='email text-orange-500'>jonatan@email.com</span> and will send updates in the run up to the event.</p>
+                        <p className="text-white text-[1.1rem] text-center leading-normal mt-6 w-2xl m-auto">We've emailed your ticket to <span className='email text-orange-500'>{email}</span> and will send updates in the run up to the event.</p>
                     </div>
                 </div>
                 <div className="ticket-image">
@@ -30,12 +42,12 @@ function Ticket({ name, email, username }) {
                     </div>
                     <div className="ticket-user flex gap-4">
                         <div className="ticket-user-pfp">
-                            <img src={Pfp} alt={name} className="rounded-2xl h-[100px] w-[100px]"/>
+                            <img src={pfp} alt={name} className="rounded-2xl h-[100px] w-[100px]"/>
                         </div>
                         <div className="ticket-user-name flex flex-col items-center justify-center">
-                            <h3 className="text-white text-2xl font-bold">Jonatan Kristof</h3>
+                            <h3 className="text-white text-2xl font-bold w-max">{name}</h3>
                             <div className="ticket-user-username">
-                                <p className="text-white">@jonatankristof0101</p>
+                                <p className="text-white text-left hover:underline cursor-pointer flex items-center" onClick={() => handleGithub(username)}><img src={GithubLogo} alt="" className="inline-block mr-1" />@{username}</p>
                             </div>
                         </div>
                     </div>
